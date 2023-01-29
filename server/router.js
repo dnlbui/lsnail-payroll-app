29cc87bfa9708fd4c267a1404cb3c82a375ede54
Daniel Bui <dnlbui@proton.me>
2023-01-29 15:51:46 -0600

    spacing

--- server/router.js
+++ server/router.js
@@ -1,8 +1,8 @@
-const Authentication = require('./controllers/authentication');
+const Authentication  = require('./controllers/authentication');
 const passportService = require('./services/passport');
-const passport = require('passport');
+const passport        = require('passport');
 
-const requireAuth = passport.authenticate('jwt', { session: false });
+const requireAuth   = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
