8ef3ec3cc24e2203e72789c3a65dd7502e9f9858
Daniel Bui <dnlbui@proton.me>
2023-01-29 14:15:07 -0600

    FE files to client folder

---
+++ server/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './features/header/header'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className= "App">
+      <Header/>
+      <div className="container">
+      </div>
+    </div>
+  );
+}
+
+export default App;
