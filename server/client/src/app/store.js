3a2fb62b3447f108ea233bd074ee518c623a9ce1
Daniel Bui <dnlbui@proton.me>
2023-02-01 18:22:46 -0600

    skeleton and commented out proxy

--- server/client/src/app/store.js
+++ server/client/src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import {emloyeesApiSlice} from '../features/api/apiSlice';
 
 export const store = configureStore({
   reducer: {
     
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(emloyeesApiSlice.middleware),
 });
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css"
-import React from 'react';
+import React, { Fragment } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -8,6 +8,9 @@ import './index.css';
 import {ApiProvider} from '@reduxjs/toolkit/query/react'
 import {productsApiSlice} from './features/api/apiSlice'
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
@@ -15,7 +18,30 @@ root.render(
   <React.StrictMode>
     <ApiProvider api={productsApiSlice}>
       <Provider store={store}>
-        <App />
+        <Router>
+          <Fragment>
+            <Nav />
+            <App>
+              <Switch>
+                <Route>
+
+                </Route>
+                <Route>
+                  
+                </Route>
+                <Route>
+                
+                </Route>
+                <Route>
+                  
+                </Route>
+                <Route>
+                
+                </Route>
+              </Switch>
+            </App>
+          </Fragment>
+      </Router>
       </Provider>
     </ApiProvider>
   </React.StrictMode>
