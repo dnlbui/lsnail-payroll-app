38b25f0341dec0d480e54e054df2b2302b9038ee
Daniel Bui <dnlbui@proton.me>
2023-02-03 12:17:30 -0600

    employeelist to employee. contains list+form now

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -6,7 +6,7 @@ import Welcome from './features/auth/Welcome'
 import RequireAuth from './features/auth/RequireAuth'
 import Register from './features/auth/Register'
 import Home from './features/home/Home'
-import EmployeesList from './features/employees/EmployeesList'
+import Employees from './features/employees/Employees'
 
 
 const App = () => {
@@ -22,7 +22,7 @@ const App = () => {
         <Route element={<RequireAuth />}>
           <Route path="welcome" element={<Welcome/>} />
           <Route path="home" element={ <Home/> } />
-          <Route path="employees" element={<EmployeesList/>} />
+          <Route path="employees" element={<Employees/>} />
           {/* <Route path="employees" element={<Employees/>} /> */}
           {/* <Route path="employees" element={<Employees/>} /> */}
         </Route>
