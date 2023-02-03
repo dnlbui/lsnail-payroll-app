0284bd20eb317dbc5bc040c237ed2fc18a8a4f20
Daniel Bui <dnlbui@proton.me>
2023-02-03 12:47:54 -0600

    Tickets skeleton. Working on ticketlist

---
+++ server/client/src/features/tickets/Tickets.js
@@ -0,0 +1,17 @@
+/* import TicketsList ;
+import TicketForm ; */
+import Nav from "../../components/NavAuth";
+import { Fragment } from 'react';
+
+const Tickets = () => {
+
+  return (
+    <Fragment>
+      <Nav/>
+{/*       <TicketsList/>
+      <TicketForm/> */}
+    </Fragment>
+  )
+}
+
+export default Tickets;
