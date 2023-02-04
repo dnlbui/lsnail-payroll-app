53b4487b0b1739c504ea21dcf6397f6ca77173ac
Daniel Bui <dnlbui@proton.me>
2023-02-04 13:35:59 -0600

    content working dates not updating bc no pass thru

--- server/client/src/features/tickets/TicketListExcerpt.js
+++ server/client/src/features/tickets/TicketListExcerpt.js
@@ -1,11 +1,8 @@
 import { Fragment } from 'react';
 import { useTicketsListQuery } from "./TicketsApiSlice";
-//import TicketCardExcerpt from "./TicketCardExcerpt";
-
-
+import TicketCardExcerpt from "./TicketCardExcerpt";
 
 const TicketList = ({name, startDate, endDate}) => {
-  
   //destructure data from EmployeeList Query
   const {
     data: queryData,
@@ -21,25 +18,16 @@ const TicketList = ({name, startDate, endDate}) => {
     content = <p>Loading...</p>
   } 
   else if (isSuccess) {
-    // content = queryData.map( element => {
-    //   return(<TicketCardExcerpt key={element._id} element={element}/>)
-    // })
-
+    content = queryData.map( element => {
+      return(<TicketCardExcerpt key={element._id} element={element}/>)
+    })
     console.log(queryData);
   } 
   else if (isError) {
     content = <p>{error}</p>
   }
   return (
-    <Fragment>
-      <section className='ticketlist'>
-        <div className='container'>
-          <div className="row row-cols-3 gy-5 offset-1">
-            {content}
-          </div>
-        </div>
-      </section>
-    </Fragment>
+  <Fragment>{content}</Fragment>
   )
 }
 
