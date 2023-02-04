38941c643f10aeb30d43297e488b8ada6faf2063
Daniel Bui <dnlbui@proton.me>
2023-02-04 13:34:25 -0600

    added a search bar redux state

--- server/client/src/app/store.js
+++ server/client/src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './api/apiSlice';
 import authReducer from '../features/auth/authSlice'
+import searchBarReducer from '../features/tickets/SearchBarSlice'
 
-
+// Configure the store
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
+    searchBar: searchBarReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true //turn off during PRODUCTION RUN
