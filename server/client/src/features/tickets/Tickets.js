c9a531d314b6fbbcac6cc59400ee8918df21d272
Daniel Bui <dnlbui@proton.me>
2023-02-03 20:17:25 -0600

    ticket list

--- server/client/src/features/tickets/Tickets.js
+++ server/client/src/features/tickets/Tickets.js
@@ -2,15 +2,26 @@
 import TicketForm ; */
 import Nav from "../../components/NavAuth";
 import { Fragment } from 'react';
-import TicketForm from "./TicketFormExcert";
+//import TicketForm from "./TicketFormExcert";
+import TicketsList from "./TicketsListExcert";
 
 const Tickets = () => {
 
   return (
     <Fragment>
       <Nav/>
-      <TicketForm/>
-      {/* <TicketsList/> */}
+      <br></br>
+      <div className="row row-cols-3 gy-10 offset-5">
+        <div className="col ">
+          <div className="jumbotron jumbotron-fluid ">
+            <div className="container">
+              <h1 className="display-8 text-center">Ticket List</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <TicketForm/> */}
+      <TicketsList/>
 
     </Fragment>
   )
--- server/client/src/features/tickets/TicketsListSearchBarExcert.js
+++ server/client/src/features/tickets/TicketsListSearchBarExcert.js
@@ -1,7 +1,79 @@
-// const {
-//   data: queryData,
-//   isLoading,
-//   isSuccess,
-//   isError,
-//   error
-// }  = useEmployeesListQuery();
+import { Form,Button, DateRangePicker, SelectPicker } from 'rsuite';
+import { Fragment, useState } from 'react';
+import { useEmployeesListQuery } from '../employees/EmployeesApiSlice';
+import { useTicketsListQuery } from './TicketsApiSlice';
+
+const TicketsListSearchBar = () => {
+  const [startDate, setStartDate] = useState(new Date('2022-02-01 00:00:00'));
+  const [endDate, setEndDate] = useState(new Date('2022-05-01 23:59:59'));
+  const [name, setName] = useState("Pick an Employee" );
+
+  const {employeeList} = useTicketsListQuery();
+
+  const handleNameInput = (event) => {setName(event); console.log(event)};
+  const handleStartDateInput = async (value) => {setStartDate(value)};
+  const handleEndDateInput = async (value) => setEndDate(value);
+
+  //destructure data from EmployeeList Query
+  const {
+    data: queryData,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  }  = useEmployeesListQuery();
+
+  //if statement that will return component depending on query status
+  let content;
+  let pickerData;
+  if(isLoading) {
+    content = <p>Loading...</p>
+  }
+  else if (isSuccess) {
+    pickerData = queryData.map( element => ({ label: element.name, value: element._id }))
+    content = (<SelectPicker label="User" data={pickerData} onSelect={(value)=>handleNameInput(value)} style={{ width: 150 }} />)
+  }
+  else if (isError) {
+    content = <p>{error}</p>
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    try{
+      employeeList({name,startDate,endDate});
+      setStartDate('');
+      setEndDate('');
+      setName('');  
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+
+  return (
+    <Fragment>
+    <div className='col-lg-7 offset-lg-3 '>
+      <Form layout="inline" onSubmit={(e)=>handleSubmit(e)}>
+        {content}
+
+        <DateRangePicker
+          format="yyyy-MM-dd hh:mm aa"
+          showMeridian
+          defaultCalendarValue={[startDate, endDate]}
+          hoverRange="week" 
+          isoWeek ranges={[]}
+          onOk={(value)=>{ handleStartDateInput(value[0]); handleEndDateInput(value[1]);}}
+        />
+
+        <Button>Submit Ticket Filter</Button>
+      </Form>
+      
+    </div>
+    <hr/>
+    </Fragment>
+    )
+}
+
+export default TicketsListSearchBar;
