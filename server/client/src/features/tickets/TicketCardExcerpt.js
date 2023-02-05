b2a1fdd7a9542d4534e0df0d297f5d04edc22cb0
Daniel Bui <dnlbui@proton.me>
2023-02-04 23:55:59 -0600

    added new col div

--- server/client/src/features/tickets/TicketCardExcerpt.js
+++ server/client/src/features/tickets/TicketCardExcerpt.js
@@ -2,13 +2,15 @@
 export default function TicketCard ({element}) {
   let {serviceDate, serviceTotal, creditCardTip, _id} = element;
   return (
-    <div className="card" value={_id} style={{width: 288}}>
-      <div className="card-body">
-        <h5 className="card-title">Service Date: {serviceDate}</h5>
-        <h5 className="card-title">Service Total: {serviceTotal}</h5>
-        <h5 className="card-title">Credit CardTip:: {creditCardTip}</h5>
+    <div className='col'>
+      <div className="card" value={_id} style={{width: 288}}>
+        <div className="card-body">
+          <h5 className="card-title">Service Date: {serviceDate}</h5>
+          <h5 className="card-title">Service Total: {serviceTotal}</h5>
+          <h5 className="card-title">Credit CardTip:: {creditCardTip}</h5>
+        </div>
+        <button type="button" value={_id} class="btn btn-danger">Delete</button>
       </div>
-      <button type="button" value={_id} class="btn btn-danger">Delete</button>
     </div>
   )
 }
