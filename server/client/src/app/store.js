aa122a6d3e6dcae69449a417a63377a02b9d5697
Daniel Bui <dnlbui@proton.me>
2023-02-04 23:54:52 -0600

    removed seachbarSlice/reducer

--- server/client/src/app/store.js
+++ server/client/src/app/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './api/apiSlice';
-import authReducer from '../features/auth/authSlice'
-import searchBarReducer from '../features/tickets/SearchBarSlice'
+import authReducer from '../features/auth/authSlice';
 
 // Configure the store
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
-    searchBar: searchBarReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true //turn off during PRODUCTION RUN
