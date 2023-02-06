f65e21bfc23497c4c41050585c1845975fabdb7e
Daniel Bui <dnlbui@proton.me>
2023-02-06 13:29:01 -0600

    added ticketlistreducer

--- server/client/src/app/store.js
+++ server/client/src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './api/apiSlice';
 import authReducer from '../features/auth/authSlice';
+import ticketListReducer from '../features/tickets/TicketListSlice';
 
 // Configure the store
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
+    ticketList: ticketListReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true //turn off during PRODUCTION RUN
