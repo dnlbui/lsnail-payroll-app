adffbecc17aa4e355ca4a23ff923ebbd78dae2e1
Daniel Bui <dnlbui@proton.me>
2023-02-07 12:27:07 -0600

    adding in keys

--- server/router.js
+++ server/router.js
@@ -3,6 +3,9 @@ const passportService    = require('./services/passport');
 const passport           = require('passport');
 const EmployeeControl    = require('./controllers/employeeControl')
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY  || require('./config/keys').STRIPE_SECRET_KEY);         
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET || require('./config/keys').STRIPE_WEBHOOK_SECRET;
+
 const requireAuth   = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
@@ -30,5 +33,48 @@ module.exports = function(app) {
   app.get('/api/ticket/', requireAuth, EmployeeControl.ticketQuery)
   //route that .get by finding the employee by ID or name... then queries for date range... then aggregates the tickets total... Then manipulate the data to get what we need
   app.get('/api/ticket/aggregation', requireAuth, EmployeeControl.aggregateTickets)
+  
+
+  //route that creates a product 
+  
+  //creates a price for the product
+  
+  //creates a customer while saving them in mongoDB
+
+  //create webhook for stripe
+  app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+    let event = request.body;
+    // Only verify the event if you have an endpoint secret defined.
+    // Otherwise use the basic event deserialized with JSON.parse
+    if (endpointSecret) {
+      // Get the signature sent by Stripe
+      const signature = request.headers['stripe-signature'];
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          signature,
+          endpointSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return response.sendStatus(400);
+      }
+    }
+  
+    // Handle the event
+    switch (event.type) {
+    case 'invoice.payment_failed':
+      const invoice = event.data.object;
+      // Then define and call a method to handle the failed payment of an Invoice.
+      // handleFailedInvoice(invoice);
+      break;
+      default:
+        // Unexpected event type
+        console.log(`Unhandled event type ${event.type}.`);
+    }
+  
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  });
   
 };
