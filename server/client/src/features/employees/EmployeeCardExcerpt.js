f7159c89cec754d1648cc45f4b2d236a00b93730
Daniel Bui <dnlbui@proton.me>
2023-02-08 14:11:14 -0600

    added delete ticket fxn

--- server/client/src/features/employees/EmployeeCardExcerpt.js
+++ server/client/src/features/employees/EmployeeCardExcerpt.js
@@ -1,18 +1,33 @@
+import { Fragment } from 'react';
+import { useDeleteEmployeeMutation } from './EmployeesApiSlice';
+
 //Returns a card for each product
 export default function EmployeeCardExcerpt ({element}) {
   let {_id, name, image} = element;
-  return (
+
+  let [deleteEmployee] = useDeleteEmployeeMutation();
+
+  const handleClick = () =>{
+    deleteEmployee(_id);
+    //refetch();
+    content = <p>Deleted</p>
+  }
+
+  let content = (
     <div className="col ">
-      <div className="card" value={_id} style={{width: 288}}>
+      <div className="card" value={_id} style={{width: 180}}>
         <img src={image} className="card-img-top" alt="..."/>
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-        </div>
         <ul className="list-group list-group-flush">
           <li className="list-group-item">Name: {name}</li>
         </ul>
-        <button type="button" value={_id} className="btn btn-danger">Delete</button>
+        <button type="button" value={_id} className="btn btn-danger" onClick={handleClick}>Delete</button>
       </div>
     </div>
+  )
+  
+  return (
+    <Fragment>
+      {content}
+    </Fragment>
   )
 }
