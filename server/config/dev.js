4a63ad0f54ac19883638d6eb05ca328c8012d4a0
Daniel Bui <dnlbui@proton.me>
2023-02-07 21:43:35 -0600

    change secret to one inside heroku

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,7 +1,7 @@
 // for dev
 module.exports = {
   MONGODB_URI: 'mongodb://127.0.0.1:27017/lsnails-db',
-  TOKEN_SECRET: 'helloworld',
+  TOKEN_SECRET: 'sdlfjasdlkf4234lkjlfk',
   STRIPE_SECRET_KEY:'sk_test_51MYtYyBxFn9jWOFEFIbu3aWCdeuTpNCnPC0cTaEbydlPxGkLoUX5FS11kmBtAmDCA5R8F2PbpYZsZb3Ca8sNp4dc003B6yNGb6',
   STRIPE_PUBLISHABLE_KEY:"pk_test_51MYtYyBxFn9jWOFEDbTYnzkAz5C9DUh4VSsDBxHzRocmhN1vVqmNgPhXkP7Map2WM7fm5viZxWoiXVmYusuzDFGi00aOKODxnK",
   STRIPE_WEBHOOK_SECRET:"whsec_196a95fc7ccb3eb3919efebbce4024c4c2b3767e0e6f9967192e35630d0b8f0b"
