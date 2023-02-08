4bd2104b7a2e082432c175c674c2c4cefabc0836
Daniel Bui <dnlbui@proton.me>
2023-02-08 11:02:02 -0600

    comment on where to add role based auth

--- server/client/src/features/auth/RequireAuth.js
+++ server/client/src/features/auth/RequireAuth.js
@@ -5,7 +5,10 @@ import { selectCurrentToken } from "./authSlice";
 const RequireAuth = () => {
   //maybee it check for local storage too?
   const token = useSelector(selectCurrentToken)
+  //useLocation hook to get the current location
   const location = useLocation()
+
+  //this is where I could add user role check***************************************
   return (
     token ? <Outlet /> : <Navigate to="/login" state={{ from: location }} replace />
   )
