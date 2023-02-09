196692262766289dba6d3193da461c8217f23634
Daniel Bui <dnlbui@proton.me>
2023-02-08 21:23:06 -0600

    fix public page to look better mobile

--- server/client/src/components/Public.js
+++ server/client/src/components/Public.js
@@ -7,32 +7,36 @@ const Public = () => {
     const content = (
         <Fragment>
             <Nav/>
-        <div className='container'>
-      <div className="row row-cols-1 gy-4 offset-3">
-        <section className="public">
-            <header>
-                <h1>Welcome to Lone Star Nails!</h1>
-            </header>
-            <main>
-                <p>Located in Fort Worth, Texas. Estalished in 2009.</p>
-                <p>&nbsp;</p>
-                <div className="row row-cols-1 gy-4 offset-2">
-                <address>
-                    Lone Star Nails<br />
-                    7812 Crowley Rd<br />
-                    Fort Worth, TX 76134<br />
-                    <a href="tel:+18176158899">(817) 615-8899</a>
-                </address>
+            <div className='container'>
+                <div className="row">
+                    <div className="col"></div>
+                        <div className="col col-6">
+                            <section className="public">
+                                <header>
+                                    <h1>Lorem Ipsum!</h1>
+                                </header>
+                                <main>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <p>&nbsp;</p>
+                                    <div className="row row-cols-1">
+                                    <address>
+                                        Ut at leo ac odio<br />
+                                        mattis vulputate nec<br />
+                                        Vestibulum, TX 3170<br />
+                                        <a href="tel:+18176158899">(464) 464 -4648</a>
+                                    </address>
+                                    </div>
+                                </main>
+                                <footer>
+                                    <div className="row row-cols-1">
+                                        <Link to="/login">Employee Login</Link>
+                                    </div>
+                                </footer>
+                            </section>
+                        </div>
+                    <div className="col"></div>
                 </div>
-            </main>
-            <footer>
-            <div className="row row-cols-1 gy-4 offset-2">
-                <Link to="/login">Employee Login</Link>
-                </div>
-            </footer>
-        </section>
-        </div>
-        </div>
+            </div>
         </Fragment>
     )
     return content
