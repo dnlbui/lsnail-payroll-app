3d1c61a1db8518971865a2da844f128c37e77130
Daniel Bui <dnlbui@proton.me>
2023-02-08 21:26:41 -0600

    removed nail salon name

--- server/client/src/components/NavAuth.js
+++ server/client/src/components/NavAuth.js
@@ -11,7 +11,7 @@ const Nav = () => {
   return (
     <nav className="navbar navbar-dark bg-dark navbar-expand-lg bg-primary">
       <div className="container-fluid">
-        <Link to="/home" className="navbar-brand"  aria-current="page">Lone Star Nails</Link>
+        <Link to="/home" className="navbar-brand"  aria-current="page">Lorem Ipsum!</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
--- server/client/src/components/NavUnAuth.js
+++ server/client/src/components/NavUnAuth.js
@@ -7,7 +7,7 @@ const Nav = () => {
   return (
     <nav className="navbar navbar-dark bg-dark navbar-expand-lg bg-primary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">Lone Star Nails</a>
+        <a className="navbar-brand" href="/">Lorem Ipsum</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
